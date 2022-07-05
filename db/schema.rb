dae410d7f4e437a7483f58f3532e6e5e4fd37908
taksas <takuminakayama4869@gmail.com>
2022-07-05 09:31:55 +0000

    top&about

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_020918) do
+ActiveRecord::Schema.define(version: 2022_07_05_020350) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2022_07_05_020918) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
