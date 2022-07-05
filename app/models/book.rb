0e44dae5e03ca40be87a1361b552a0205c41409f
taksas <takuminakayama4869@gmail.com>
2022-07-05 06:51:38 +0000

    Header_and_Footer

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
 
   has_one_attached :image
 
+
+  belongs_to :user
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
+  get 'home/about' => 'homes#about'
 
   resources :books, only: [:new, :index, :show]
   resources :users, only: [:show, :edit]
